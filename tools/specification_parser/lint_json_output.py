e86a4a626a428ef897fc98ec752e07c9078b4108
Justin Abrahms <jabrahms@ebay.com>
2022-05-29 19:36:56 -0700

    Linting to ensure we don't have duplicated numbers

--- tools/specification_parser/lint_json_output.py
+++ tools/specification_parser/lint_json_output.py
@@ -9,10 +9,17 @@ def main(f):
     with open(f) as jsonfile:
         spec = json.load(jsonfile)
         entries = [];
+        machineid_seen = []
         for entry in spec['rules']:
+            machineid_seen += [entry['machine_id']]
             entries.append(entry)
             for child in entry.get('children', []):
                 entries.append(child)
+
+        if len(machineid_seen) != len(set(machineid_seen)):
+            duped = set([x for x in machineid_seen if machineid_seen.count(x) > 1])
+            print(f"There were duplicated machine ids in the list. This means two things claim to be the same rule number. Duplicated thing: {duped}", file=sys.stderr)
+
         try:
             for entry in entries:
                 if entry.get('RFC 2119 keyword') is None and \
